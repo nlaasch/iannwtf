42d4dfa721589c4972a8098008c7fcfbc514cf86
Fabio-Klinge <93822163+Fabio-Klinge@users.noreply.github.com>
2021-11-07 23:58:41 +0100

    Final commit

--- Homework 2/mlp_final.py
+++ Homework 2/mlp_final.py
@@ -1,10 +1,16 @@
 import numpy as np
-import random
+#import random
 from homework2 import sigmoid, sigmoidprime, generate_data
 from matplotlib import pyplot as plt
 import time
 
+
+
+
+# Possible choices for binary input
 input_dataset = np.array([[0,0],[0,1],[1,0],[1,1]])
+
+# List of bools as labels for respective logical operator
 label_and = np.array([0,0,0,1])
 label_or = np.array([0,1,1,1])
 label_nand = np.array([1,1,1,0])
@@ -12,45 +18,96 @@ label_nor = np.array([1,0,0,0])
 label_xor = np.array([0,1,1,0])
 
 
+
+
 class Perceptron:
+    """
+    Instantiates a single perceptron with own weights and bias.
+
+    Parameters:
+    input_units(int): Length of the input array. Weights get created accordingly.
+    """
+
+
     def __init__(self, input_units):
+        """
+        Saves all important variables for the perceptron. 
+        Also initializes its weights and random bias.
+
+        Parameters:
+        input_units(int): Length of the input array. Weights get created accordingly.
+        """
+
         self.input_units = input_units
         self.bias = np.random.randn()
+
+        # Learning rate
         self.alpha = 1
+        # To be accessed for backpropagation
         self.drive = 0
+        # Saves input
         self.inputs = 0
+
+        # Random weight for each input
         self.weights = np.random.randn(input_units)
 
-    #Calculates forward step using the sigmoid function
+
     def forward_step(self, inputs):
-        #print("Data Input: " + str(inputs))
+        """
+        Calculates forward step using sigmoid activation function. 
+        Passes inputs through perceptron.
+
+        Parameters:
+        inputs(arr/list): Size 2 with binary boolean input representing logical statement.
+        """
+
+        # Multiply weights with given inputs
         self.drive = self.weights @ inputs + self.bias
+        # Store it
         self.inputs = inputs
-        ##print("Drive: " + str(self.drive))
-        ##print("Calculated forward step: " + str(sigmoid(self.drive)))
+
+        # Perceptron output
         return sigmoid(self.drive)
 
+
     def update(self, delta):
+        """
+        Calculated error-signal from backpropagation is used to update weights and bias.
+
+        Parameters:
+        delta(float): error signal computed in MLP -> backprop_step
+        """
+
         #Compute gradients
         gradients = delta * self.inputs
 
-        #print("Gradients: " + str(gradients))
-
-        #Update parameters
-        #print("Previous Weights: " + str(self.weights))
+        #Update parameters 
         self.bias -= self.alpha * delta
         self.weights -= self.alpha * gradients
-        #print("Updated Weights: " + str(self.weights))
-
 
 
 
 
 class MLP:
+    """
+    Contains multiple "layers" of perceptron objects.
+    Passes inputs through the network and computes the error-signal with backpropagation
+    """
+
+
     def __init__(self):
+        """
+        Initializes one hidden & one output layer.
+        Stores important variables.
+        """
+        # Initializes layers expecting specific input sizes
         self.hiddenlayer = [Perceptron(2) for i in range(4)]
         self.outputneuron = Perceptron(4)
+
+        # Network output
         self.output = 0
+
+        # For measuring network performance
         self.accuracy_sum = 0
         self.loss = []
         self.accuracies = []
@@ -58,12 +115,30 @@ class MLP:
 
 
     def forward_step(self, inputs):
+        """
+        Pushes given inputs through each perceptron.
+
+        Parameters:
+        inputs(arr/list): Size 2 with binary boolean input representing logical statement.
+        """
+
+        # Call every perceptron in hl with given inputs
         calculated = np.array([perceptron.forward_step(inputs) for perceptron in self.hiddenlayer])
+        # Pass hl output to output layer
         self.output = self.outputneuron.forward_step(calculated)
 
     def backprop_step(self, target):
+        """
+        Compute error-signal with backpropagation.
+
+        Parameters:
+        target: The ground truth. Output the network is supposed to have.
+        """
+
+        # Applies function to compute error signal for output layer
         delta = -(target - self.output) * sigmoidprime(self.outputneuron.drive)
 
+        # Updates neuron in output layer
         self.outputneuron.update(delta)
 
 
@@ -77,14 +152,19 @@ class MLP:
 
 
     def train(self, epoch):
+        """
+        Trains network.
 
+        Parameters:
+        epoch(int): Training repetitions.
+        """
 
         #Loss
         error = 0
         outputs = []
 
 
-
+        # Trains network
         for datapoint in range(input_dataset.shape[0]):
             self.forward_step(input_dataset[datapoint])
             self.backprop_step(label_xor[datapoint])
@@ -98,18 +178,20 @@ class MLP:
                 self.accuracy_sum += 1
             outputs.append(output)
 
-
+        # Measure performance
         self.accuracies.append(round(self.accuracy_sum/((epoch * 4) + 1), 4))
         self.loss.append(error)
-
         elapsed = (time.time() - self.start)
 
-
+        # Output
         return [outputs, elapsed, epoch + 1, self.accuracies[-1], self.loss[-1]]
 
 
 
 def plot(epochs, loss, accuracy):
+    """
+    Plots output
+    """
     fig, (ax1, ax2) = plt.subplots(2,1,figsize = (14,12))
 
     fig.suptitle("Evolution of networks prediction", fontsize=16)
@@ -130,7 +212,7 @@ def plot(epochs, loss, accuracy):
     plt.show()
 
 
-
+# Runs the network
 if __name__ == "__main__":
 
 
